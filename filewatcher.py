32a7f8bf938fedf9dccf7066810e13e5905a5788
Sunglow38 <alec.holdren@gmail.com>
2018-10-17 08:55:06 -0400

    Initial commit

---
+++ filewatcher.py
@@ -0,0 +1,49 @@
+import sys
+import glob
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class Watcher:
+    DIRECTORY_TO_WATCH = r'C:\Users\Alec\data'
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            h = hash(event.src_path)
+            print(h)
+            print("Received created event - %s." % event.src_path)
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print("Received modified event - %s." % event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    w.run()
+
+
